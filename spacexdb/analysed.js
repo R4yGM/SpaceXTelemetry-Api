b96d2854a19aa0f2525dec692150c0799d186c7f
R4yan <yessou.rayan@gmail.com>
2020-06-29 20:16:10 +0200

    Update analysed.js

--- spacexdb/analysed.js
+++ spacexdb/analysed.js
@@ -1,1 +1,27 @@
-
+function main(){
+var uri = window.location;
+ if(uri.includes("?")){
+  var payload = uri.split('?')[1];
+   if(payload.includes("mission_name=")){
+     var mission_name = payload.replace("mission_name=")
+     fetch('https://spacextelemetry.cf/spacexdb/analysed.json')
+    .then(response => response.json())
+    .then(data => {
+     var result = JSON.parse(data);
+       for(var index in result.pages){
+         if(result.pages[index].mission_name == mission_name){
+         var resp = `{"mission_name":\"${mission_name}\","spacexdb_link":\"${result.pages[index].spacexdb_link}\","published":${result.pages[index].published}`
+         document.documentElement.innerHTML = resp;
+         }
+       }
+     });
+   }
+ }else{
+ 
+fetch('https://spacextelemetry.cf/spacexdb/analysed.json')
+  .then(response => response.json())
+  .then(data => {
+    document.documentElement.innerHTML = data;
+    });
+  }  
+}
